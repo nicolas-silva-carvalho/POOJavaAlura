880ebd058fa5d99c6e394c089b68332f3b5de8d9
Nicolas <nicolas_carv@outlook.com>
2023-09-06 00:04:08 -0300

    first commit

---
+++ src/Principal.java
@@ -0,0 +1,7 @@
+import br.com.alura.POOJava.modelos.Filme;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme filme = new Filme();
+    }
+}
---
+++ src/br/com/alura/POOJava/modelos/Filme.java
@@ -0,0 +1,24 @@
+package br.com.alura.POOJava.modelos;
+
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+
+    int getTotalDeAvaliacoes(){
+        return totalDeAvaliacoes;
+    }
+
+    void exibeFichaTecnica(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+}
